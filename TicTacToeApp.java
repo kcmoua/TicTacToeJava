63464f0691fd30791bf9560bc91b8023bbaeb303
Keng Moua <keng@Kengs-MacBook-Pro.local>
2020-06-03 16:28:05 -0400

    working code, need to refactor duplicate code

--- TicTacToeApp.java
+++ TicTacToeApp.java
@@ -12,14 +12,19 @@ public class TicTacToeApp {
 		System.out.println("Welcome to Tic-Tac-Toe Player X and Player O! Player X will go first.");
 		Scanner scan = new Scanner(System.in);	// open scanner
 				
-		do {	// playAgain loop
-			while(gameIsOver==false) {	// play the game while game is not over
+		do {	// playAgain do-while loop
+			while(gameIsOver==false) {	// main game function code
 				// X turn
-				System.out.println();
-				game1.displayBoard();
-				System.out.print("\n'X', choose your location (row, column): ");
-				playerXRow = scan.nextInt();
-				playerXCol = scan.nextInt();
+				do {
+					System.out.println();
+					game1.displayBoard();
+					System.out.print("\n'X', choose your location (row, column): ");
+					playerXRow = scan.nextInt();
+					playerXCol = scan.nextInt();
+					if(game1.isValid(playerXRow, playerXCol)==false) {
+						System.out.println("\nYour numbers are not valid. Try again.");
+					}
+				} while(game1.isValid(playerXRow, playerXCol)==false);
 				game1.playMove('X', playerXRow, playerXCol);
 						
 				if(game1.isWinner('X') == true) {
@@ -34,11 +39,16 @@ public class TicTacToeApp {
 					break;
 				}	
 				// O turn
-				System.out.println();
-				game1.displayBoard();
-				System.out.print("\n'O', choose your location (row, column): ");
-				playerORow = scan.nextInt();
-				playerOCol = scan.nextInt();
+				do {
+					System.out.println();
+					game1.displayBoard();
+					System.out.print("\n'O', choose your location (row, column): ");
+					playerORow = scan.nextInt();
+					playerOCol = scan.nextInt();
+					if(game1.isValid(playerXRow, playerXCol)==false) {
+						System.out.println("\nYour numbers are not valid. Try again.");
+					}
+				} while(game1.isValid(playerORow, playerOCol)==false);
 				game1.playMove('O', playerORow, playerOCol);
 						
 				if(game1.isWinner('O') == true) {
@@ -49,12 +59,12 @@ public class TicTacToeApp {
 				}
 				// X is always the first player, O will never have a chance to be the last move before a tie
 			}
-			gameIsOver = false;	// reset value so the game can be played again if applicable
+			gameIsOver = false; // reset value so the game can be played again if applicable
 			game1.clearBoard(); // reset board so the game can be played again if applicable
 			game1.resetTurns(); // reset turns so the game can be played again if applicable
 			System.out.print("\nWould you like to play again? (y/n): ");	// ask if the user wants to play again
-			playAgain = scan.next().charAt(0);
-		} while(playAgain=='y');
+			playAgain = scan.next().toLowerCase().charAt(0);	//store the user's input 'y' or 'n' into playAgain
+		} while(playAgain=='y');	// run the program again while playAgain = 'y'
 		System.out.println("\nThanks for playing!");
 		scan.close();
 	}
